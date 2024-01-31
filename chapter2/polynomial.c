717f422aeed93d18aea54253197ed6f064162831
Diego <diegoforlongnell@gmail.com>
2024-01-31 19:50:11 -0300

    used Horner's rule to better performance

--- chapter2/polynomial.c
+++ chapter2/polynomial.c
@@ -4,7 +4,7 @@ int main(void){
 	int x,val;
 	printf("Enter the value of x: ");
 	scanf("%d",&x);
-	val = 3*(x*x*x*x*x)+2*(x*x*x*x)-5*(x*x*x)-1*(x*x)+7*(x)-6;
+	val = ((((3*x+2)*x-5)*x-1)*x+7)*x-6;
 	printf("The polynomial 3(x^5)+2(x^4)-5(x^3)-1(x^2)+7(x)-6 evaluated in %d is: %d\n",x,val);
 	return 0;
 }
