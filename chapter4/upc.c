0ade129bd9cdace08db6feda170a1e21866a9d8c
Diego <diegoforlongnell@gmail.com>
2024-02-04 11:56:34 -0300

    now the program checks if the code is valid or not

--- chapter4/upc.c
+++ chapter4/upc.c
@@ -1,12 +1,17 @@
 #include<stdio.h>
 
 int main(void){
-	int d,i1,i2,i3,i4,i5,j1,j2,j3,j4,j5,total;
+	int i0,i1,i2,i3,i4,i5,j1,j2,j3,j4,j5,digit,total;
 	printf("Enter the digits: ");
-	scanf("%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d",&d,&i1,&i2,&i3,&i4,&i5,&j1,&j2,&j3,&j4,&j5);
-	
-	total = (-1*(i1+i3+i5+j2+j4+3*(d+i2+i4+j1+j3+j5)-1)%10)+9;
-	printf("Check digit: %d\n",total);
+	scanf("%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d",
+		&i0,&i1,&i2,&i3,&i4,&i5,&j1,&j2,&j3,&j4,&j5,&digit);	
+	total = (-1*(i1+i3+i5+j2+j4+3*(i0+i2+i4+j1+j3+j5)-1)%10)+9;
+	if(total==digit){
+		printf("VALID\n");
+	}
+	else {
+		printf("NOT VALID\n");
+	}
 
 	return 0;
 }
